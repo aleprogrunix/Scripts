330b965fd9249f14b7eb748a5619ee0d46236ca5
aleprogrunix <alegonpo90@gmail.com>
2020-04-19 17:37:05 +0200

    Arreglado script para pasar archivos .rsc a antena mikrotik y que lo ejecute con run-after-reset con python

--- Python/prueba-config.py
+++ Python/prueba-config.py
@@ -10,52 +10,56 @@ ssh_usuario = 'admin'
 ssh_clave = ''
 ssh_puerto = 22     # En este caso usamos el 22, pero se puede usar el necesario.
 
-# Creamos esta variable que nos permitirá abrir las conexiones ssh.
+# Creamos esta variable que nos permitirá abrir las conexiones ssh y sftp.
 ssh_client = paramiko.SSHClient()
 
 # Función que nos permitirá conectarnos por ssh
 def conectando():
     try:
-            # Para pasar el archivo por SFTP tenemos que tener anterior mente una conexión por SSH.
-             # ssh_client = paramiko.SSHClient()
+            # Para pasar el archivo por SFTP tenemos que tener anteriormente una conexión por SSH.
+            # ssh_client = paramiko.SSHClient()
             ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh_client.connect(hostname=ssh_cliente, port=ssh_puerto, username=ssh_usuario, password=ssh_clave)
 
             print ('¡Conexión establecida satisfactoria!')
             
-    except EnvironmentError:
+    except:
 
             print ('Conexión fallida... ')
     
 
 # Función que nos permitirá subir el archivo a la antena.
-def upload():
+def upload1():
     # Una vez conectado tenemos que elegir que archivo vamos a pasar de nuestra ruta local a la ruta remota.
-    localPath = '/home/alejandro/Documentos/Python/Ejercicios/configfile-v-6.43.rsc' # Ruta local y archivo a enviar.
-    remotePath= '/configfile-v-6.43.rsc' # Ruta remota a donde quiero ingresar el archivo elegido.
+    localPath = '/home/alejandro/Documentos/python/Ejercicios/configfile.rsc' # Ruta local y archivo a enviar. 
+    remotePath= '/configfile.rsc' # Ruta remota a donde quiero ingresar el archivo elegido.
     try:
             # abrimos conexion sftp.
             subida = ssh_client.open_sftp()
             # Cargando archivo
             subida.put(localPath, remotePath)
+
             print ('¡Archivo subido con exito!')
         
-    except EnvironmentError:
+    except:
             print ('Lo sentimos, hubo un error al subir el archivo')    
+
 
 ##########EJECUTAMOS LAS FUNCIONES###################
 conectando()
-upload()
+upload1()
 #####################################################
 
 # Ejecutamos el comando de forma remota para borrar los parametros capturando entrada, salida y error estándar
-entrada, salida, error = ssh_client.exec_command('system reset-configuration run-after-reset=configfile-v-6.43.rsc')
+entrada, salida, error = ssh_client.exec_command('system reset-configuration no-defaults=yes run-after-reset=configfile.rsc')
 
 # Mostramos la salida estándar en pantalla
 print (salida.read())
 
-print ('Reiniciando...')  
-time.sleep(60)
+# Detenemos un tiempo el script para que le de tiempo a la entena a resetearse y reiniciarse.
+print ('Reseteando y reiniciando...')  
+time.sleep(25) # 25 segundos son suficiente.
 
 # Cerramos la conexión
 ssh_client.close()
+
